19612fc71c2b407bb8bc20740df146175b852c0d
Maria Fernandez de Moura Ferro <mariafmferro@gmail.com>
2022-11-24 18:54:28 -0300

    Adequação de arquivos

--- src/data/connection.ts
+++ src/data/connection.ts
@@ -3,16 +3,14 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const connection = knex ({
-    client: "mysql",
+export const connection = knex({
+    client: 'mysql',
     connection: {
         host: process.env.DB_HOST,
-        port: 3306,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_SCHEMA,
+        port: 3306,
         multipleStatements: true
     }
 })
-
-export default connection
--- src/data/migrations.ts
+++ src/data/migrations.ts
@@ -1,29 +1,27 @@
-import connection from "./connection"
-import users from "./users.json"
+import { connection } from "./connection"
 
 const printError = (error: any) => { console.log(error.sqlMessage || error.message) }
 
 const createTables = () => connection
    .raw(`
 
-      CREATE TABLE IF NOT EXISTS aula_webservices_users (
-         id VARCHAR(255) PRIMARY KEY,
-         name VARCHAR(255) NOT NULL,
-         nickname VARCHAR(255) NOT NULL,
-         email VARCHAR(255) UNIQUE NOT NULL,
-         address VARCHAR(255) NOT NULL
+      CREATE TABLE IF NOT EXISTS ex_servicos_backend (
+         id INT AUTO_INCREMENT PRIMARY KEY,
+         nome VARCHAR(255) NOT NULL,
+         cep VARCHAR(8) NOT NULL,
+         logradouro VARCHAR(255) NOT NULL,
+         numero VARCHAR(255) NOT NULL,
+         complemento VARCHAR(255),
+         bairro VARCHAR(255) NOT NULL,
+         cidade VARCHAR(255) NOT NULL,
+         estado VARCHAR(2) NOT NULL
       );
    `)
    .then(() => { console.log("Tabelas criadas") })
    .catch(printError)
 
-const insertUsers = () => connection("aula_webservices_users")
-   .insert(users)
-   .then(() => { console.log("Usuários criados") })
-   .catch(printError)
 
 const closeConnection = () => { connection.destroy() }
 
 createTables()
-   .then(insertUsers)
    .finally(closeConnection)
